3a102aab14e713e7d3cab7928b5d0d22949ac0b4
Jaishree Bala <jaishree1bala@gmail.com>
2020-12-31 00:50:55 -0500

    Upload photo front end completed

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,19 +1,73 @@
 import React, { useState, useRef } from 'react'
 import ImageUploader from "react-images-upload";
+import Error from '../components/Error';
+import { useHistory } from 'react-router-dom'
 
-function Post() {
+
+
+function Post({ isLoggedIn }) {
     const [pictures, setPictures] = useState([]);
+    const [errors, setErrors] = useState("");
+
+    const history = useHistory()
     const description = useRef();
     const tags = useRef();
 
-    const onDrop = picture => {
-        setPictures([...pictures, picture]);
-    };
+    const bearer = `Bearer ${localStorage.getItem('token')}`
 
+    const onDrop = picture => {
+        setPictures([picture]);
+    };
+    const uploadpictureHandler = async () => {
+        if (isLoggedIn) {
+            if (!pictures[0]) {
+                setErrors("Please Upload A Photo");
+            } else
+                if (!(description.current.value.length > 0 && tags.current.value.length > 0)) {
+                    // console.log(description)
+                    // console.log(description.length)
+                    setErrors("Please add a description and tags for the photo.");
+                }
+                else {
+                    console.log("succ ess")
+                    try {
+                        let bodydata = new FormData();
+                        bodydata.append('photo', pictures[0][0])
+                        bodydata.append('description', description.current.value)
+                        bodydata.append('tags', tags.current.value)
+                        const response = await fetch(`api/v1/photo`, {
+                            method: "POST",
+                            body: bodydata,
+                            headers: {
+                                'Authorization': bearer
+                                // 'Content-Type': "application/json; charset=UTF-8"
+                            }
+                        })
+                        const data = await response.json();
+                        setErrors("");
+                        if (data.success) {
+                            console.log("success")
+                            history.push("/feed");
+                        }
+                        else {
+                            setErrors(data.error)
+                        }
+
+                    } catch (err) {
+                        setErrors(err)
+                    }
+                }
+        }
+        else {
+            // history.push("/login");
+        }
+    }
     return (
         <div className="post">
+            <Error error={errors} />
             <ImageUploader
-                name="file"
+                name="photo"
+                className="photo"
                 withIcon={true}
                 onChange={onDrop}
                 imgExtension={[".jpg", ".gif", ".png", ".svg"]}
@@ -38,6 +92,7 @@ function Post() {
                     </span>
                 </label>
             </div>
+            <div className="button" onClick={uploadpictureHandler}>Post</div>
         </div>
     )
 }
