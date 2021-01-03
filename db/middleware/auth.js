e7b1f8bce086ea3f6494f168c5df1acce2f12112
Jaishree Bala <jaishree1bala@gmail.com>
2021-01-03 03:33:50 -0500

    Updated README.md

--- db/middleware/auth.js
+++ db/middleware/auth.js
@@ -7,12 +7,11 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
     let token;
 
-
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(' ')[1];
-    } else if (req.cookies.token) {
-
+    if (req.cookies.token) {
         token = req.cookies.token;
+    }
+    else if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
     }
     if (!token) {
         return next(new ErrorResponse('Please login/register to perform this action', 401));
